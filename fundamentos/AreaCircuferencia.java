c9089345023cdc4d35af4d6cf6a31764c1e65050
Jefferson Santos <59482366+jeffersonnasciment0@users.noreply.github.com>
2021-10-07 19:32:06 -0300

    updated

--- fundamentos/AreaCircuferencia.java
+++ fundamentos/AreaCircuferencia.java
@@ -4,18 +4,10 @@ public class AreaCircuferencia {
 	
 	
 	public static void main(String[] args) {
-		/* System.out.println(2+3+4); */
-		/*
-		 * Como definimos uma variável em java ? 
-		 * 1. definimos seu tipo
-		 * 2. nome da variável
-		 * 3. valor  
-		 */
-		
-//		int raio = 3;
+	
+
 		double raio2 = 3.4;
-		final double PI = 3.14159; // final -> é utlizado para definimos uma constante / Por convenção todas constantes são com Letras maiúsculas 
-		
+		final double PI = 3.14159; 
 		double area = PI*raio2*raio2;
 		
 		System.out.println("Resultado da Area = " + area + " M2");
--- fundamentos/operadores/Ternario.java
+++ fundamentos/operadores/Ternario.java
@@ -3,12 +3,10 @@ package fundamentos.operadores;
 public class Ternario {
 
 	public static void main(String[] args) {
-		
+//		Utilizando operador Ternario de treino 
 		double media = 5.6;
-		
-		String resultadoRecuperacao = media >= 5.0 ? "em recuperação !" : "reprovado !";
+		String resultadoRecuperacao = media >= 5.0 ? "em recuperaï¿½ï¿½o !" : "reprovado !";
 		String resultado = media >= 7.0 ? "Aprovado !!" : resultadoRecuperacao;
-		
 		System.out.println(resultado);
 	}
 }
