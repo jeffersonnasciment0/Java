1bab922a2ea86d5bc7d5049f79552ec5d5d79630
Jefferson Santos <59482366+jeffersonnasciment0@users.noreply.github.com>
2021-12-09 13:05:43 -0300

    add project "sistema de contribuintes"

---
+++ sistema de contribuintes/sistema/src/nf/PessoasFisicas.java
@@ -0,0 +1,13 @@
+package nf;
+
+public class PessoasFisicas extends Pessoa{
+	
+	public double gastoSaude;
+	
+	
+	public PessoasFisicas(String nome, double rendaAnual) {
+		super.setNome(nome);
+		super.setRendaAnual(rendaAnual);
+	}
+	
+}
---
+++ sistema de contribuintes/sistema/src/nf/Teste.java
@@ -0,0 +1,9 @@
+package nf;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new PessoaFisicas("Joao", 65000);
+	}
+}
